08cc2d890f2e0e4df503cd56be70e63809c30c08
Dom <97745392+Domagoj-S@users.noreply.github.com>
2022-03-08 22:35:02 +0100

    Commiting changes

--- app.js
+++ app.js
@@ -21,18 +21,30 @@ window.onload = () => {
   for (let i = numberOfQuestions -1; i >= 0; i--) {
     
     console.log(i);
-    console.log(questionArray[questionCounter] + `... (${numberOfQuestions} questions left)`);
+    console.log(questionArray[questionCounter] + `... (${numberOfQuestions -1} questions left)`);
     userInputs.push(prompt(questionArray[questionCounter] + `... (${numberOfQuestions -1} questions left)`));
+   
+    if (userInputs[questionCounter] == ""){
+      console.log("The user didn't write anything");
+      
+        } else {
+    
     console.log(userInputs);
+   
+  }
+
     questionCounter++;
     
    console.log(`Question Counter: ${questionCounter}`);
     numberOfQuestions--;
+
+    
     
   }
 
+
   // The modified story
-  let originalStory = `<p>In ${userInputs[0]}, traveller extrodinaire ${userInputs[1]} found themselves at ${userInputs[2]} when they met an old schoolmate from  ${userInputs[3]}. Well, as they were talking a hole opened up in the ground and swallowed them whole! That was the end of ${userInputs[1]} and their friend!"</p>`
+  let originalStory = `<h1 style="color:red; text-shadow: 1px 1px 4px black">In ${userInputs[0]}, traveller extrodinaire ${userInputs[1]} found themselves at ${userInputs[2]} when they met an old schoolmate from  ${userInputs[3]}. Well, as they were talking a hole opened up in the ground and swallowed them whole! That was the end of ${userInputs[1]} and their friend!"</h1>`
   console.log(originalStory);
 
  
